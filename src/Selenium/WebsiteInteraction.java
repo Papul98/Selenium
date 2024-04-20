5d9940071dbdee0771185b4c488a1975ec54da92
Papul98 <160265971+Papul98@users.noreply.github.com>
2024-04-20 11:44:29 +0530

    Update WebsiteInteraction.java

--- src/Selenium/WebsiteInteraction.java
+++ src/Selenium/WebsiteInteraction.java
@@ -17,7 +17,7 @@ public class WebsiteInteraction {
 		
 		driver.manage().window().maximize();
 		 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		 Thread.sleep(2000);
+		 Thread.sleep(5000);
 		 
 	}
 	 
